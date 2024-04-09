0db36525e23316bedd63cbf67b79dd744213ca84
Andrey Fedoseev <MaxviP100@yandex.ru>
2024-04-09 17:25:02 +0700

    message 'hello'

--- main.py
+++ main.py
@@ -2,3 +2,16 @@ import telebot
 
 bot = telebot.TeleBot('5859808636:AAFmC9v8iOpYIV7CuY5HWwbqJBMzFTw4kpo')
 
+
+@bot.message_handler(commands=['start'])
+def main(message):
+    bot.send_message(message.chat.id, 'Hello')
+
+
+@bot.message_handler()
+def info(message):
+    if message.text.lower() == 'id':
+        bot.reply_to(message, f'ID: {message.from_user.id}')
+
+
+bot.polling(none_stop=True)
